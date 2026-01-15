bcf4bd33b5255420242094ec2120a3526bb49f02
Mateoooo <mateofranchesconunezmayo@gmail.com>
2026-01-15 14:12:12 +0100

    parte2-ejercicio6_clase

---
+++ app/src/main/java/es/fpsumma/dam2/api/ui/screen/tareas/ListadoTareasRemoteRoute.kt
@@ -0,0 +1,32 @@
+package es.fpsumma.dam2.api.ui.screen.tareas
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import es.fpsumma.dam2.api.ui.navegation.Routes
+import es.fpsumma.dam2.api.viewmodel.TareasRemoteViewModel
+
+@Composable
+fun ListadoTareasRemoteRoute(
+    navController: NavController,
+    vm: TareasRemoteViewModel,
+    modifier: Modifier = Modifier,
+) {
+    LaunchedEffect(Unit) {
+        vm.loadTareas()
+    }
+
+    val state by vm.state.collectAsState()
+
+    ListadoTareasContent(
+        state = state,
+        onBack = { navController.popBackStack() },
+        onAdd = { navController.navigate(Routes.TAREA_ADD) },
+        onOpenDetalle = { id -> navController.navigate(Routes.tareaView(id)) },
+        onDelete = {},
+        modifier = modifier,
+    )
+}
